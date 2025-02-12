0169b644076ba0827fb3dd722791dd5622401ef8
Shahid Chaudhary <shahidchaudhary52@gmail.com>
2025-02-12 22:46:18 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/logo1.jpg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <>
+      <nav className=" mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+          <img src={logo} className="mx-2 w-30 h-20" alt="logo" />
+        </div>
+        <div className="m-8 flex justify-center items-center gap-4 text-2xl color-white ">
+          <FaLinkedin />
+          <FaGithub />
+          <FaInstagram />
+          <FaSquareXTwitter />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
